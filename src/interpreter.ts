85b9147fdb0a6bb2cc52085c1b990836c016e5c4
Mike Warot <chezmike@gmail.com>
2025-05-27 23:37:41 -0500

    Create interpreter.ts

---
+++ src/interpreter.ts
@@ -0,0 +1,7 @@
+// The PermEq interpreter (stub)
+import { Environment } from './environment';
+import type { Statement } from './ast';
+
+export function interpret(statements: Statement[], env: Environment) {
+  // TODO: Implement interpretation logic
+}
